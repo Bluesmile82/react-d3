0f9c28c9bde16dbde317d8bb67ee498e077d9163
Bluesmile82 <pursuance@gmail.com>
2016-06-23 15:28:25 +0200

    Starts authentication and request for mindmaps

--- src/App.js
+++ src/App.js
@@ -1,102 +1,80 @@
 import React, { Component } from 'react';
 import d3 from 'd3';
+import $ from "jquery";
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      time: 0
+      mindmaps: []
     }
   }
 
-  componentDidMount(){
-    this.appendDiv();
-    setInterval( this.tick.bind(this) , 1000 );
+  signIn() {
+  //   response = RestClient.post( auth_url , email: 'admin@test.com', password: "trycatch", password_confirmation: "trycatch")
+  // selected_headers = response.headers.slice( :access_token , :token_type, :client, :expiry, :uid)
+    const host2 = "ideas-test-api.herokuapp.com";
+    const host = "localhost:3002";
+    const auth_url = "http://admin:trycatch@" + host + "/auth/sign_in";
+    const username = "admin";
+    const password = "trycatch";
+
+    $.post({
+      beforeSend: function(xhr) { xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)); },
+      url: auth_url,
+      type: "POST",
+      dataType: 'json',
+      data: {
+        email: "admin@test.com",
+        password:"trycatch",
+        password_confirmation: "trycatch"
+      },
+      error: function (err) {
+      console.log(err);
+      },
+      success: function (resp) {
+         console.log("hello");
+         console.log(resp);
+         console.log(resp.getResponseHeader("Access-Token"));
+      }
+    });
   }
 
-  collisionDetection(){
-    var width = 960,
-        height = 500;
 
-    var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; }),
-        root = nodes[0],
-        color = d3.scale.category10();
+  componentDidMount() {
+    this.signIn();
+  }
 
-    root.radius = 0;
-    root.fixed = true;
+  requestMindmaps(){
+    const host2 = "ideas-test-api.herokuapp.com";
+    const old_url = this.props.source;
+    const host = "localhost:3002";
+    const url = "http://admin:trycatch@" + host + "/api/v1/mindmaps";
 
-    var force = d3.layout.force()
-        .gravity(0.05)
-        .charge(function(d, i) { return i ? 0 : -2000; })
-        .nodes(nodes)
-        .size([width, height]);
-
-    force.start();
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    svg.selectAll("circle")
-        .data(nodes.slice(1))
-      .enter().append("circle")
-        .attr("r", function(d) { return d.radius; })
-        .style("fill", function(d, i) { return color(i % 3); });
-
-    force.on("tick", function(e) {
-      var q = d3.geom.quadtree(nodes),
-          i = 0,
-          n = nodes.length;
-
-      while (++i < n) q.visit(collide(nodes[i]));
-
-      svg.selectAll("circle")
-          .attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+    $.get({
+      url: url,
+      type: "GET",
+      dataType: 'json',
+      data: {},
+      error: function (err) {
+      },
+      success: function (resp) {
+         console.log("hello");
+         console.log(resp);
+      }
     });
+  }
 
-    svg.on("mousemove", function() {
-      var p1 = d3.mouse(this);
-      root.px = p1[0];
-      root.py = p1[1];
-      force.resume();
-    });
-
-    function collide(node) {
-      var r = node.radius + 16,
-          nx1 = node.x - r,
-          nx2 = node.x + r,
-          ny1 = node.y - r,
-          ny2 = node.y + r;
-      return function(quad, x1, y1, x2, y2) {
-        if (quad.point && (quad.point !== node)) {
-          var x = node.x - quad.point.x,
-              y = node.y - quad.point.y,
-              l = Math.sqrt(x * x + y * y),
-              r = node.radius + quad.point.radius;
-          if (l < r) {
-            l = (l - r) / l * .5;
-            node.x -= x *= l;
-            node.y -= y *= l;
-            quad.point.x += x;
-            quad.point.y += y;
-          }
-        }
-        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-      };
-    }
-}
-
-  tick(){
-    this.setState({ time: this.state.time + 1 });
+  componentWillUnmount() {
+    this.serverRequest.abort();
   }
 
   render() {
     return (
       <div>
         <h1>Hello, world.</h1>
-          <p className="lead"> Hello, you've been here { this.state.time } secs </p>
+          <p className="lead"> { this.state.mindmaps } </p>
       </div>
 
     );
